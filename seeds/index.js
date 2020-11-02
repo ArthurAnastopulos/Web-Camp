033cae53d8a35363880cb8859557ac32706dfcb0
Arthur Anastopulos <arthuranastopulos@hotmail.com>
2020-11-02 10:44:29 -0300

    Generate random seeds of campgrounds

---
+++ seeds/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/web-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log('Database conneceted');
+});
+
+const sample = arr => arr[Math.floor(Math.random() * arr.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const rand = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[rand].city}, ${cities[rand].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        camp.save();
+    }
+};
+
+seedDB();
