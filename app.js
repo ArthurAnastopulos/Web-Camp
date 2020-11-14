0b50131f9611a0d918d43b6cc40cba68c052091a
Arthur Anastopulos <arthuranastopulos@hotmail.com>
2020-11-14 09:42:57 -0300

    Serving static Assets

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const reviews = require('./routes/reviews');
 mongoose.connect('mongodb://localhost:27017/web-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -33,38 +34,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-// const validateReview = (req, res, next) => {
-//     const result = reviewSchema.validate(req.body);
-//     if (result.error) {
-//         const msg = error.details.map(el => el.message).join(',');
-//         throw new ExpressError(msg, 400);
-//     } else {
-//         next();
-//     }
-// };
-
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home');
 });
-
-// app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-//     const campground = await Campground.findById(req.params.id);
-//     const review = new Review(req.body.review);
-//     campground.reviews.push(review);
-//     await review.save();
-//     await campground.save();
-//     res.redirect(`/campgrounds/${campground._id}`);
-// }));
-
-// app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-//     const { id, reviewId } = req.params;
-//     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-//     await Review.findByIdAndDelete(reviewId);
-//     res.redirect(`/campgrounds/${id}`);
-// }));
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page not Found', 404));
