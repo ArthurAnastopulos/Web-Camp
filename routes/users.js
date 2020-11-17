be1b9a341324f02f9e3beb7d16b7f6abfd71743b
Arthur Anastopulos <arthuranastopulos@hotmail.com>
2020-11-17 09:32:16 -0300

    Added login route

--- routes/users.js
+++ routes/users.js
@@ -21,4 +21,13 @@ router.post('/register', catchAsync(async (req, res) => {
     }
 }));
 
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    res.redirect('/campgrounds');
+});
+
 module.exports = router;
