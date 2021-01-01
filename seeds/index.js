ddb142f0606b98b1f740f218ac3e51f21e376e5d
Arthur Anastopulos <arthuranastopulos@hotmail.com>
2021-01-01 13:40:16 -0300

    Added image upload

--- seeds/index.js
+++ seeds/index.js
@@ -10,6 +10,7 @@ mongoose.connect('mongodb://localhost:27017/web-camp', {
 });
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log('Database conneceted');
@@ -27,12 +28,23 @@ const seedDB = async () => {
             author: '5fb52f326c97c525ae941c0d',
             location: `${cities[rand].city}, ${cities[rand].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'The Nature is just like flowers in the forest, but camping is fire that will lit Nature true colors.',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                },
+                {
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
+                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                }
+            ]
         });
-        camp.save();
+        await camp.save();
     }
 };
 
-seedDB();
+seedDB().then(() => {
+    mongoose.connection.close();
+})
